5d28c33dd968017b8b794787c535a5889ad1c468
Yamamoto <info@ind-ex.tokyo>
2026-05-12 02:10:51 +0900

    コミットメッセージ

--- app.js
+++ app.js
@@ -638,13 +638,13 @@ function renderAboutPage() {
     <div class="about-body">
       <h2 class="about-section-title">WHAT IS NARRATIVE</h2>
       <p>
-        いつも行くコンビニのあの店員さんが、<br class="br-pc"/><br class="br-sp"/>子どもの頃なにを見ていたか。<br class="br-pc"/><br class="br-sp"/>電車で隣に座ったその人が、<br class="br-pc"/><br class="br-sp"/>これまでにどんな修羅場を潜り抜けたのか。<br class="br-pc"/><br class="br-sp"/>この街の名物おじさんが、<br class="br-pc"/><br class="br-sp"/>最近なにに感動して泣いたのか。
+        いつも行くコンビニのあの店員さんが<br class="br-pc"/><br class="br-sp"/>子どもの頃なにを見ていたか。<br class="br-pc"/><br class="br-sp"/>電車で隣に座ったその人が<br class="br-pc"/><br class="br-sp"/>これまでにどんな修羅場を潜り抜けたのか。<br class="br-pc"/><br class="br-sp"/>この街の名物おじさんが<br class="br-pc"/><br class="br-sp"/>最近なにに感動して泣いたのか。
       </p>
       <p>
-       すれ違うだけではたぶん知ることのない、<br class="br-sp"/>その人のこれまでのこと。
+       普段通り過ぎている誰もが、<br class="br-sp"/>誰かである誰かで、<br class="br-pc"/><br class="br-sp"/>今日に至るまでの物語があります。
       </p>
       <p>
-        子どもの頃になにを見て、昨日なにを考えて、<br class="br-pc"/><br class="br-sp"/>明日なにをしようとしているのか。<br class="br-pc"/><br class="br-sp"/>そんな人たちの、すこし個人的な話を<br class="br-sp"/>聞きにいくメディアです。
+        NARRATIVEは、<br class="br-sp"/>そんな人たちの超個人的な話を<br class="br-pc"/><br class="br-sp"/>聞いていくメディアです。
       </p>
 
       <div class="about-section-divider">📮</div>
